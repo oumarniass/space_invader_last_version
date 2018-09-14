81ad60318c9d97bb61b08e64fabea44c88cc8646
oumarniass <sowoumarousmane@gmail.com>
2018-09-14 18:39:37 +0200

    project_space invaders dernieres version

---
+++ JS/fichier.js
@@ -0,0 +1,331 @@
+
+var Alien = function (aType, align, aCol) {
+    this.type = aType;
+    this.points = 40 - 10 * aType;
+    this.line = align;
+    this.column = aCol;
+    this.alive = true;
+    this.height = 20;
+    this.width = 28;
+    this.positionX = 100 + this.width * this.column;
+    this.positionY = 100 + 30 * this.line;
+    this.direction = 1;
+    this.state = 0;
+
+    this.changeEtat = function () {  //change l'etat de 2 alien different
+        this.state = !this.state ? 20 : 0;
+    };
+
+    this.down = function () { //redescend apre avoir changer la direction
+        this.positionY = this.positionY + 10;
+
+    };
+
+    this.move = function () { //  definit un nouv position apre le deplacema et affiche l'inver
+        if (this.positionY >= Game.height - 50) {
+            Game.over();
+        }
+        this.positionX = this.positionX + 5 * Game.direction;
+        this.changeEtat();
+        if (this.alive) this.draw();
+    };
+
+    this.checkCollision = function () { //verifie si ennemi est ttuer par colission
+        if (Gun.ray.active == true && this.alive == true) {
+            if ((Gun.ray.positionX >= this.positionX && Gun.ray.positionX <= (this.positionX + this.width)) && (Gun.ray.positionY >= this.positionY && Gun.ray.positionY <= (this.positionY + this.height))) {
+                this.kill();
+                Gun.ray.destroy();
+            }
+        }
+    };
+
+    this.draw = function () { //atire l'ennemi ds  nouv posiition
+        if (this.alive) { //nouv position
+            canvas.drawImage(
+                pic,
+                this.width * (this.type - 1),
+                this.state,//etat
+                this.width,
+                this.height,
+                this.positionX,
+                this.positionY,
+                this.width,
+                this.height);
+        } else if (this.alive == null) { //afficher l'explosion
+            canvas.drawImage(
+                pic,
+                85,
+                20,
+                28,
+                20,
+                this.positionX,
+                this.positionY,
+                this.width,
+                this.height);
+            this.alive = false; //cacher ennemi apre mort
+        }
+    };
+
+    this.kill = function () { //tue ennemi
+        this.alive = null;
+        canvas.clearRect(this.positionX, this.positionY, this.width, this.height);
+        Game.refreshScore(this.points);
+    }
+};
+
+Gun = {
+    position: 220,
+    toleft: false,
+    toright: false,
+
+    init: function () { //INITIALISER LE PISTOLLET ET SON MOUVEMENT
+        this.draw();
+        this.toLeft();
+        this.toRight();
+        setInterval("Gun.toLeft()", 30);
+        setInterval("Gun.toRight()", 30);
+    },
+
+    draw: function () { // TIRER AVK LE PISTOOLLET
+        canvas.drawImage(pic, 85, 0, 28, 20, this.position, 470, 28, 20);
+    },
+
+    fire: function () { //shot COUP
+        this.ray.create();
+    },
+
+    toLeft: function () { // DEPLACER le vaisseau avk LE PISTOLLET A GAUCHE
+        if (this.toleft) {
+            if (this.position - 5 > 0) {
+                canvas.clearRect(0, 472, Game.width, 28);
+                this.position -= 5;
+                this.draw();
+            }
+        }
+    },
+
+    toRight: function () { // DEPLACER LE vaisseau avk le PISTOLLET A DROIT
+        if (this.toright) {
+            if (this.position + 30 < Game.width) {
+                canvas.clearRect(0, 472, Game.width, 28);
+                this.position += 5;
+                this.draw();
+            }
+        }
+    },
+
+    ray: { //ball
+        positionX: 0,
+        positionY: 465,
+        length: 5,
+        speed: 15,
+        animation: null,
+        active: false,
+        create: function () { // creer une ball s'il exist po
+            if (!this.active) {
+                this.positionX = Gun.position + 14;
+                this.active = true;
+                this.animation = setInterval("Gun.ray.animate()", this.speed);
+            }
+
+        },
+        animate: function () { //transition du ball
+            this.positionY -= this.length;
+            if (this.positionY <= 5) this.destroy();
+            else {
+                Game.drawAliens();
+                this.draw();
+            }
+        },
+        draw: function () { // trajectoir de la ball verification colision avk enemi
+            if (this.active) {
+                canvas.beginPath();
+                canvas.strokeStyle = 'white';
+                canvas.lineWidth = 2;
+                canvas.moveTo(this.positionX, this.positionY);
+                canvas.lineTo(this.positionX, this.positionY + this.length);
+                canvas.stroke();
+
+                for (i = 0; i < 5; i++) {
+                    for (j = 0; j < 11; j++) {
+                        Game.aliens[i][j].checkCollision();
+                    }
+                }
+            }
+        },
+        destroy: function () { //detruit la ball
+            this.positionY = 465;
+            this.active = false;
+            clearInterval(this.animation);
+            this.animation = null;
+            Game.drawAliens();
+        },
+    }
+
+};
+
+Game = {
+    types: [1, 2, 3, 2, 3], //definir des ennemi different
+    aliens: [
+        [11],
+        [11],
+        [11],
+        [11],
+        [11]
+    ],
+    height: 0,
+    width: 0,
+    interval: 0,
+    intervalDefault: 1000,
+    direction: 1,
+    animation: null,
+    alives: 1,
+    score: 0,
+    level: 1,
+
+    init: function (aWidth, aHeight) { //initialiser les position par defautt des ennemi et nbr de vie
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 11; j++) {
+                this.aliens[i][j] = new Alien(this.types[i], i, j);
+                this.alives++;
+                this.aliens[i][j].draw();
+            }
+        }
+        this.width = aWidth;
+        this.height = aHeight;
+        this.play();
+        Gun.init();
+        this.refreshScore(0);
+        document.getElementById('level').innerHTML = this.level;
+        document.getElementById('inter').innerHTML = this.interval;
+    },
+
+    changeDirection: function () { // change la diirection gauche a droit
+        if (this.direction == 1) {
+            this.direction = -1;
+        } else {
+            this.direction = 1;
+        }
+    },
+    clearCanvas: function () { // effacer le canevas mais pas d'arme
+        canvas.clearRect(0, 0, this.width, this.height - 28);
+    },
+    closeToLeft: function () { // verifier si un alien a atteint le cote gauche
+        return (this.aliens[0][0].positionX - 10 < 0) ? true : false;
+    },
+    closeToRight: function () { //verifier si un alien a atteint le cote gauche
+        return (this.aliens[4][10].positionX + 35 > this.width) ? true : false;
+    },
+    drawAliens: function () { //creer un enemis
+        this.clearCanvas();
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 11; j++) {
+                this.aliens[i][j].draw();
+            }
+        }
+    },
+    animate: function () { // mouvement ennemi
+        this.clearCanvas();
+        Gun.ray.draw();
+        this.checkAliens();
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 11; j++) {
+                this.aliens[i][j].move();
+            }
+        }
+        if (this.closeToLeft() || this.closeToRight()) {
+            this.changeDirection();
+            for (i = 0; i < 5; i++) {
+                for (j = 0; j < 11; j++) {
+                    this.aliens[i][j].down();
+                }
+            }
+            this.increaseSpeed();
+        }
+    },
+
+    play: function () { //commencer le jeux
+        this.interval = this.intervalDefault;
+        this.interval = this.interval - (this.level * 20);
+        this.animation = setInterval("Game.animate()", this.interval);
+    },
+    increaseSpeed: function (newInterval) { //AUGUMENTER LA VITESSE
+        clearInterval(this.animation);
+        if (newInterval === undefined) this.interval = this.interval - 10;
+        else this.interval = newInterval;
+
+        this.animation = setInterval("Game.animate()", this.interval);
+        document.getElementById('inter').innerHTML = this.interval;
+    },
+    onkeydown: function (ev) { //EVENEMENT CLE EN BAS
+        if (ev.keyCode == 37) Gun.toleft = true;
+        else if (ev.keyCode == 39){ Gun.toright = true;var player = document.querySelector('#audioPlayer');
+            player.play();}
+        else if (ev.keyCode == 32){ Gun.fire();var player1 = document.querySelector('#audioPlayer1');
+            player1.play();}
+        else return;
+    },
+    onkeyup: function (ev) { //EVENEMENT CLE EN HAUT
+        if (ev.keyCode == 37) Gun.toleft = false;
+        else if (ev.keyCode == 39) Gun.toright = false;
+        else return;
+    },
+    over: function () { //COMMENCER LE JEU
+        clearInterval(this.animation);
+        canvas.clearRect(0, 0, this.width, this.height);
+        canvas.font = "40pt Calibri,Geneva,Arial";
+        canvas.strokeStyle = "rgb(FF,0,0)";
+        canvas.fillStyle = "rgb(0,20,180)";
+        canvas.strokeText("Game Over", this.width / 2 - 80, this.height / 2 - 20);
+    },
+    checkAliens: function () { //check number of aliens
+        if (this.alives == 0){
+            alert("Bravo Vouz avez gagner \n passer au niveau Suivant");
+            this.nextLevel();
+        }
+        else if (this.alives == 1) this.increaseSpeed(80 - (this.level * 20));
+        else if (this.alives <= 10) this.increaseSpeed(100 - (this.level * 20));
+        else if (this.alives <= 10) this.increaseSpeed(200 - (this.level * 20));
+        else if (this.alives <= 25) this.increaseSpeed(300 - (this.level * 20));
+    },
+    refreshScore: function (points) { //display the score
+        this.alives--;
+        this.score += points;
+        document.getElementById('score').innerHTML = this.score;
+        document.getElementById('alives').innerHTML = this.alives;
+    },
+    nextLevel: function () {
+        //resurect aliens
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 11; j++) {
+                this.aliens[i][j].alive = true;
+                this.alives++;
+            }
+        }
+        clearInterval(this.animation);
+        this.level++;
+        document.getElementById('level').innerHTML = this.level;
+        this.play();
+        this.increaseSpeed(this.interval);
+        document.getElementById('inter').innerHTML = this.interval;
+    }
+};
+
+//define the global context of the game
+var element = document.getElementById('aliensCanvas');
+if (element.getContext) {
+    var canvas = element.getContext('2d');
+
+    var pic = new Image();
+    pic.src = 'imagess/sprite.png';
+
+    Game.init(530, 500);
+
+    document.body.onkeydown = function (ev) {
+        Game.onkeydown(ev);
+    };
+    document.body.onkeyup = function (ev) {
+        Game.onkeyup(ev);
+    };
+}
+
